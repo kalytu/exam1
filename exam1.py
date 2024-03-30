c79e58ff79fcd1e4c1b43ff96f527a45cb10b331
iammarkin <104249953+iammarkin@users.noreply.github.com>
2024-03-30 11:38:49 +0200

    Update exam1.py

--- exam1.py
+++ exam1.py
@@ -12,4 +12,16 @@ def main():
     words = load_words()
     current_word = choose_word(words)
     guessed_letters = []
-    attempts = 6    
+    attempts = 6
+    
+    while attempts > 0:
+        display_word = ""
+        for letter in current_word:
+            if letter in guessed_letters:
+                display_word += letter + " "
+            else:
+                display_word += "_ "
+        print("слово:", display_word)
+        print("кількість спроб:", attempts)
+       
+        guess = input("введіть літеру: ").lower()
