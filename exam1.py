a0ce280ec0f5745fd6f0e6ab09ccb8256bc6b60d
kalytu <104250492+kalytu@users.noreply.github.com>
2024-04-05 22:06:38 +0300

    Update exam1.py

--- exam1.py
+++ exam1.py
@@ -24,4 +24,28 @@ def main():
         print("слово:", display_word)
         print("кількість спроб:", attempts)
        
-        guess = input("введіть літеру: ").lower()
+       try:
+            guess = input("Введіть літеру: ").lower()
+
+            if len(guess) != 1 or not guess.isalpha():
+                raise ValueError("Будь ласка, введіть лише одну літеру")
+
+            if guess in guessed_letters:
+                print("Ви вже вгадали цю літеру!")
+            elif guess in current_word:
+                print("Вітаю! Ви вгадали літеру.")
+                guessed_letters.append(guess)
+                if all(letter in guessed_letters for letter in current_word):
+                    print("Вітаю! Ви перемогли! Слово було '{}'.".format(current_word))
+                    break
+            else:
+                print("На жаль, такої літери немає у слові.")
+                attempts -= 1
+        except ValueError as ve:
+            print(ve)
+
+    if attempts == 0:
+        print("На жаль, ви програли. Слово було '{}'.".format(current_word))
+
+if __name__ == "__main__":
+    main()
